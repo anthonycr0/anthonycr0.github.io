199b7f97cb7cd6ce5c0273f0d09ad402c4e67ac1
Anthony Roberts <anthonycr0@users.noreply.github.com>
2019-07-10 22:18:18 +1000

    Updates

---
+++ precache-manifest.54c0f4fa58f5850f679e7d0142b51b21.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107d4a067e88d50b315a1dc43429dc16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0423196ff66287797f03",
+    "url": "/static/css/main.0f8af3bf.chunk.css"
+  },
+  {
+    "revision": "934aa198e280e5e53c6c",
+    "url": "/static/js/2.cbd4e2b1.chunk.js"
+  },
+  {
+    "revision": "0423196ff66287797f03",
+    "url": "/static/js/main.0bdc3e5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2928664fe1fc6aca88583a6f606d60ba",
+    "url": "/static/media/app-store-badge.2928664f.svg"
+  },
+  {
+    "revision": "83ab4944eee9c61f97b03ada3032a852",
+    "url": "/static/media/apple-tv-channels.83ab4944.png"
+  },
+  {
+    "revision": "db529c574a37462d2eccb763111e5acc",
+    "url": "/static/media/apple-tv-guide.db529c57.png"
+  },
+  {
+    "revision": "ee825dbdd23b51cc98de70b73504a77d",
+    "url": "/static/media/bg-desktop.ee825dbd.jpg"
+  },
+  {
+    "revision": "726641b004ef2f4de24c5f323174d993",
+    "url": "/static/media/code-config.726641b0.png"
+  },
+  {
+    "revision": "093c486042969ac79e5711784cc19e09",
+    "url": "/static/media/design-components.093c4860.png"
+  },
+  {
+    "revision": "5ab5b37fff15ede6cc079dad6c91c501",
+    "url": "/static/media/design-history.5ab5b37f.png"
+  },
+  {
+    "revision": "e1d138041021018662453d5c10f2f0e9",
+    "url": "/static/media/design-margins.e1d13804.png"
+  },
+  {
+    "revision": "00cdc4947c36018ccb881c7d8017e410",
+    "url": "/static/media/design-sketch.00cdc494.png"
+  },
+  {
+    "revision": "3dcfd9f94e4bf7d1f4032257ed439073",
+    "url": "/static/media/finder-dark.3dcfd9f9.png"
+  },
+  {
+    "revision": "534fa455f6652f799dd9314fee676ef1",
+    "url": "/static/media/finder-light.534fa455.png"
+  },
+  {
+    "revision": "70cfbaccb7d93e525981287abed5b81e",
+    "url": "/static/media/in-n-out.70cfbacc.jpg"
+  },
+  {
+    "revision": "4355fa5495f49d16885d60fce37c9a9e",
+    "url": "/static/media/ios-isometric.4355fa54.png"
+  },
+  {
+    "revision": "3914a04301fd7d1441dae4bf05e6ec9c",
+    "url": "/static/media/macos-concept.3914a043.png"
+  },
+  {
+    "revision": "a2fb40bd342ab59587abc828416b0251",
+    "url": "/static/media/me.a2fb40bd.jpg"
+  },
+  {
+    "revision": "bac7a81b82c0b6f3f70d380df7677f30",
+    "url": "/static/media/play-store-badge.bac7a81b.svg"
+  },
+  {
+    "revision": "8ccf794e12cf2157f06784ae6ff2ea66",
+    "url": "/static/media/profile-photo.8ccf794e.jpeg"
+  },
+  {
+    "revision": "eeae11637dbaa2663c95493410323a87",
+    "url": "/static/media/quick-maths-promo.eeae1163.png"
+  },
+  {
+    "revision": "721d0c12be9c8bb2ee74dcced4bab7b7",
+    "url": "/static/media/site-schedule.721d0c12.png"
+  },
+  {
+    "revision": "4232e254ba842104a2b04be88d03ba04",
+    "url": "/static/media/streamee-header.4232e254.png"
+  },
+  {
+    "revision": "9ad0bc4a0f5fbae379851b270fd9c2e7",
+    "url": "/static/media/unihack-header.9ad0bc4a.png"
+  },
+  {
+    "revision": "8608872087ae9e18c1d42ddd2b21e073",
+    "url": "/static/media/website-designs.86088720.png"
+  },
+  {
+    "revision": "69adffa2d18bac31532feb7e94105c4d",
+    "url": "/static/media/website-original.69adffa2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c2328109cfd708c4382805e02521964.js"
+  "/precache-manifest.54c0f4fa58f5850f679e7d0142b51b21.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
